6ba6218e6a054e7f68852f16d6578652fa124059
Marco89G <marco89.g@gmail.com>
2025-02-03 14:35:27 +0800

    Additional code mods
    Additional code mods to the image GUI

--- Image_Controller.py
+++ Image_Controller.py
@@ -65,6 +65,26 @@ class ImageController:
         image = self.model.get_tk_photoimage()
         self.view.display_image(image)
 
+    def on_scale_change(self, value):
+        """
+        Handles scaling the current image.
+    
+        Sets the scale factor in the model and scales the image.
+        The scaled image is then displayed in the view.
+    
+        Returns:
+            None
+        """
+        print("Scaling image")
+        # print(f"ImageController.on_scale_change(): Scaling image by: {value}")
+        # Convert value returned from the slider control from text value to a float fractional value
+        scale_factor = float(float(value)/100.0)
+        self.model.set_scale_factor(scale_factor)
+        # Get the scaled image as a PhotoImage
+        tk_img = self.model.get_edited_scaled_image_as_tk()
+        # Update the view with the scaled image
+        self.view.update_edited_image(tk_img)
+        
     def save_image(self):
         # Handle saving image
         print("Saving image")
@@ -101,3 +121,4 @@ class ImageController:
         # Handle setting window size
         print("Setting window size")
         pass
+    
